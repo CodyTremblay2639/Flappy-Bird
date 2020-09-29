58f3b5a8d9aec082a42fe1a1e19e7b4d5f05eab6
CodyTremblay2639 <72087458+CodyTremblay2639@users.noreply.github.com>
2020-09-29 10:32:39 -0400

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -1,32 +1,33 @@
-// planning on updating to a class
-function Pipe() {
-  this.x = width + 50;
-  this.space = 200;
-  this.top = random(5,height - this.space);
-  this.bottom = height - (this.space + this.top);
-  this.w = 50;
-  this.speed = 2;
-  this.destroy = false;
+class Pipe {
+  constructor() {
+    this.x = width + 50;
+    this.space = 200;
+    this.top = random(5,height - this.space);
+    this.bottom = height - (this.space + this.top);
+    this.w = 50;
+    this.speed = 2;
+    this.destroy = false;
+  }// constructor
   
-  this.show = function() {
+  show() {
     fill(255);
     rect(this.x,0,this.w,this.top);
     rect(this.x,height - this.bottom,this.w,this.bottom);
-  }
+  }// show
   
-  this.move = function() {
+  move() {
     this.x -= this.speed;
     if (this.x < -50){
       this.destroy = true;
       pipes.push(new Pipe());
     }
-  }
+  } // move
   
-  this.hit = function(bird) {
+  hit(bird) {
     if (bird.x + bird.w > this.x && bird.x + bird.w < this.x + this.w){
       if (bird.y - bird.w < this.top || bird.y + bird.w > height - this.bottom){
         bird.death = true;
       }
     }
-  }
+  }// hit
 }
